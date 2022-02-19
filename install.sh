4eb15bf51979f14f1d6c4e835f824540b24ed6d0
thomas10-10 <36126914+thomas10-10@users.noreply.github.com>
2022-02-19 21:44:11 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd /opt
 sudo curl -L "https://github.com/socle-py/socle/releases/download/$(basename $(curl -fsSLI -o /dev/null -w %{url_effective} https://github.com/socle-py/socle/releases/latest))/socle.tar.gz" | sudo tar -xz 
-sudo ln -f -s /opt/socle/socle /usr/local/bin/socle
+sudo ln -f -s /opt/socle/socle /usr/local/bin/socle.py
 
 sudo mkdir -p  /usr/local/share/socle
 sudo curl -Lo /usr/local/share/socle/socle.yml https://raw.githubusercontent.com/socle-py/SOCLE/main/socle.yml
